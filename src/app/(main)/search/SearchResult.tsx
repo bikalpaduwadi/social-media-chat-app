9f3c91fe02c97f952d9e09bb3a2bcf32569a4408
Bikalpa Duwadi <bikalpa.duwadi@gmail.com>
2024-08-24 15:53:14 +0545

    Implement search feature

---
+++ src/app/(main)/search/SearchResult.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React from "react";
+import { Loader2 } from "lucide-react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+import kyInstance from "@/lib/ky";
+import { PostsPage } from "@/types/post";
+import Post from "@/components/posts/Post";
+import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
+import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
+
+interface SearchResultsProps {
+  query: string;
+}
+
+const SearchResults = ({ query }: SearchResultsProps) => {
+  const {
+    data,
+    status,
+    isPending,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
+    queryKey: ["post-feed", "search", query],
+    queryFn: ({ pageParam }) =>
+      kyInstance
+        .get("/api/search", {
+          searchParams: {
+            q: query,
+            ...(pageParam ? { cursor: pageParam } : {}),
+          },
+        })
+        .json<PostsPage>(),
+    initialPageParam: null as string | null,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+    gcTime: 0,
+  });
+
+  const posts = data?.pages.flatMap((page) => page.posts) || [];
+
+  if (isPending) {
+    return <PostsLoadingSkeleton />;
+  }
+
+  if (status === "success" && !posts.length && !hasNextPage) {
+    return (
+      <div className="text-center text-muted-foreground">
+        No posts found for this query.
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <p className="text-center text-destructive">
+        Something went wrong while loading search posts
+      </p>
+    );
+  }
+
+  return (
+    <InfiniteScrollContainer
+      onBottomReached={() => {
+        hasNextPage && !isFetching && fetchNextPage();
+      }}
+      className="space-y-5"
+    >
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+
+      {isFetchingNextPage && <Loader2 className="mx-auto my-3 animate-spin" />}
+    </InfiniteScrollContainer>
+  );
+};
+
+export default SearchResults;
---
+++ src/app/api/search/route.ts
@@ -0,0 +1,65 @@
+import { NextRequest } from "next/server";
+
+import prisma from "@/lib/prisma";
+import { PostsPage } from "@/types/post";
+import { validateRequest } from "@/utils/auth";
+import { getPostDataInclude } from "@/utils/prisma";
+
+const SEARCH_PAGE_SIZE = 5;
+
+export async function GET(req: NextRequest) {
+  try {
+    const q = req.nextUrl.searchParams.get("q") || "";
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const searchQuery = q.split(" ").join(" & ");
+
+    const { user } = await validateRequest();
+
+    if (!user) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const posts = await prisma.post.findMany({
+      where: {
+        OR: [
+          {
+            content: {
+              search: searchQuery,
+            },
+          },
+          {
+            user: {
+              displayName: {
+                search: searchQuery,
+              },
+            },
+          },
+          {
+            user: {
+              username: {
+                search: searchQuery,
+              },
+            },
+          },
+        ],
+      },
+      include: getPostDataInclude(user.id),
+      orderBy: { createdAt: "desc" },
+      take: SEARCH_PAGE_SIZE + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+    });
+
+    const nextCursor =
+      posts.length > SEARCH_PAGE_SIZE ? posts[SEARCH_PAGE_SIZE].id : null;
+
+    const data: PostsPage = {
+      posts: posts.slice(0, SEARCH_PAGE_SIZE),
+      nextCursor,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
